1ef8a8677f42c3bb5f7f505b3388494ee9c86125
Milad Nekofar <milad@nekofar.com>
2024-01-20 09:51:38 +0330

    test: replace `Counter` test file by typescript version

--- test/Counter.t.ts
+++ test/Counter.t.ts
@@ -1,6 +1,6 @@
-const { expect } = require("chai");
-const hre = require("hardhat");
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("Counter contract", function () {
     async function CounterLockFixture() {
