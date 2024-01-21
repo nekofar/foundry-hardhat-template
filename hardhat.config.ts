5ca3366f05d87422baf7e1913784bd55c60352c4
Milad Nekofar <milad@nekofar.com>
2024-01-21 15:11:42 +0330

    chore: change pragma solidity version from 0.8.13 to 0.8.18
    fixup! chore: change pragma solidity version from 0.8.13 to 0.8.18

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-foundry";
 import "@nomiclabs/hardhat-solhint";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
 };
 
 export default config;
