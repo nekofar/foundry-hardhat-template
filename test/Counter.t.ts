62dedb4622bd7a75e7ed0135f4987e19b5ed3daa
Milad Nekofar <milad@nekofar.com>
2025-06-03 13:27:34 +0300

    refactor(test): reformat `Counter` tests for consistent style
    updated indentation for improved readability and adherence to style guidelines.

--- test/Counter.t.ts
+++ test/Counter.t.ts
@@ -3,23 +3,23 @@ import { viem } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 
 describe("Counter contract", function () {
-    async function CounterLockFixture() {
-        const counter = await viem.deployContract("Counter");
-        await counter.write.setNumber([0n]);
+  async function CounterLockFixture() {
+    const counter = await viem.deployContract("Counter");
+    await counter.write.setNumber([0n]);
 
-        return { counter };
-    }
+    return { counter };
+  }
 
-    it("Should increment the number correctly", async function () {
-        const { counter } = await loadFixture(CounterLockFixture);
-        await counter.write.increment();
-        expect(await counter.read.number()).to.equal(1n);
-    });
+  it("Should increment the number correctly", async function () {
+    const { counter } = await loadFixture(CounterLockFixture);
+    await counter.write.increment();
+    expect(await counter.read.number()).to.equal(1n);
+  });
 
-    // This is not a fuzz test because Hardhat does not support fuzzing yet.
-    it("Should set the number correctly", async function () {
-        const { counter } = await loadFixture(CounterLockFixture);
-        await counter.write.setNumber([100n]);
-        expect(await counter.read.number()).to.equal(100n);
-    });
+  // This is not a fuzz test because Hardhat does not support fuzzing yet.
+  it("Should set the number correctly", async function () {
+    const { counter } = await loadFixture(CounterLockFixture);
+    await counter.write.setNumber([100n]);
+    expect(await counter.read.number()).to.equal(100n);
+  });
 });
